858b12cc3d60b94ab666db156c18ba1293f00a03
Akshat <akshat26april@gmail.com>
2023-03-01 15:02:48 +0530

    Updated Projects Section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,6 +12,123 @@ const Projects = () => {
             Projects
           </h1>
 
+          {/*Weather App*/}
+
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">
+              Dynamic Weather App using ReactNative
+            </h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                02/2023 - 02/2023
+              </h3>
+
+              <li className="text-sm md:text-base text-justify">
+                Developed a Dynamic Weather App using HTML5, CSS3, ReactNative
+                and OpenWeather API which shows information like current weather
+                condition with icon and text, Realtime Temperature, Feels Like
+                Temperature, Wind Speed, Humidity & Visibility of current
+                location using geolocation module.
+                <br />
+                <br />
+                Click on the Live Demo button to get deatils of weather at your
+                location.
+              </li>
+              <br />
+              <div className="flex flex-row gap-5 ">
+                <a
+                  className="group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointershadow-gray-700 shadow-gray-700 shadow-lg duration-300 hover:scale-105"
+                  href={"https://weather-app-akshat.netlify.app/"}
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+                <a
+                  className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
+                  href={"https://github.com/akshat26akd/Weather-App"}
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/*Currency Converter*/}
+
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">
+              Realtime Currency Converter using ReactJs and Material UI
+            </h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                01/2023 - 01/2023
+              </h3>
+
+              <li className="text-sm md:text-base">
+                Developed Realtime Currency Converter using ReactJs, Material UI
+                and Freecurrency API where you can covert currency of all major
+                countries into each other with realtime exchange rates.
+                <br />
+                <br />
+                I've also exported the APK version of it for Android Devices.
+                <br />
+                <br />
+                Click on the Live Demo button to convert any currency.
+              </li>
+              <br />
+              <div className="flex flex-row gap-5 ">
+                <a
+                  className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
+                  href={"https://currency-converter-realtime.netlify.app/"}
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+                <a
+                  className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
+                  href={
+                    "https://github.com/akshat26akd/Realtime-Currency-Converter"
+                  }
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/*Portfolio Website*/}
+
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">
+              Portfolio Website using ReactJs and Tailwind CSS
+            </h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                11/2022 - 12/2022
+              </h3>
+
+              <li className="text-sm md:text-base">
+                Developed this personal portfolio website using latest
+                technologies like ReactJs, Tailwind CSS etc.
+              </li>
+              <br />
+              <a
+                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
+                href={
+                  "https://github.com/akshat26akd/Portfolio-Using-React-Tailwind"
+                }
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
+
           {/*Image Colorization*/}
 
           <div>
@@ -21,7 +138,7 @@ const Projects = () => {
             </h2>
 
             <div className="flex flex-col justify-between items-baseline">
-              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400 ">
                 01/2022 - 05/2022
               </h3>
 
@@ -31,13 +148,13 @@ const Projects = () => {
               </li>
               <br />
               <a
-                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer"
+                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
                 href={
                   "https://github.com/akshat26akd/Image-Colorization-Using-GANs"
                 }
                 target="_blank"
               >
-                Github Link
+                GitHub
               </a>
             </div>
           </div>
@@ -61,42 +178,13 @@ const Projects = () => {
               </li>
               <br />
               <a
-                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer"
+                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
                 href={
                   "https://github.com/akshat26akd/Respiratory-Disease-Detection"
                 }
                 target="_blank"
               >
-                Github Link
-              </a>
-            </div>
-          </div>
-
-          {/*Portfolio*/}
-
-          <div>
-            <h2 className="pt-10 text-xl md:text-3xl font-bold">
-              Portfolio Website using ReactJs and Tailwind CSS
-            </h2>
-
-            <div className="flex flex-col justify-between items-baseline">
-              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
-                11/2022 - 12/2022
-              </h3>
-
-              <li className="text-sm md:text-base">
-                Developed a personal portfolio website using latest technologies
-                like ReactJs, Tailwind CSS etc.
-              </li>
-              <br />
-              <a
-                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer"
-                href={
-                  "https://github.com/akshat26akd/Portfolio-Using-React-Tailwind"
-                }
-                target="_blank"
-              >
-                Github Link
+                GitHub
               </a>
             </div>
           </div>
