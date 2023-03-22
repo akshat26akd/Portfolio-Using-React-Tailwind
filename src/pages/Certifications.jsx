21c881d66fc8b0fdd500c910e8eb0bd62269c34e
Akshat <akshat26april@gmail.com>
2023-03-22 21:54:14 +0530

    Added Some New Certifications

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -13,7 +13,10 @@ const Certifications = () => {
           </h1>
 
           {/* BODY */}
-
+          <h2 className="text-3xl md:text-4xl italic font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+            <br />
+            <br /> Web Technologies
+          </h2>
           {/*HTML, CSS, and Javascript for Web Developers*/}
 
           <div>
@@ -23,7 +26,7 @@ const Certifications = () => {
 
             <div className="flex flex-col justify-between items-baseline">
               <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
-                Coursera
+                Coursera | John Hopkins University
               </h3>
 
               <ul className="text-sm md:text-base">
@@ -49,7 +52,7 @@ const Certifications = () => {
 
             <div className="flex flex-col justify-between items-baseline">
               <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
-                Coursera
+                Coursera | Meta
               </h3>
 
               <ul className="text-sm md:text-base">
@@ -65,7 +68,9 @@ const Certifications = () => {
               </a>
             </div>
           </div>
-
+          <h2 className="text-3xl md:text-4xl italic font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+            <br /> Machine Learning Specialization
+          </h2>
           {/*Programming for Everybody (Getting Started with Python)*/}
 
           <div>
@@ -75,7 +80,7 @@ const Certifications = () => {
 
             <div className="flex flex-col justify-between items-baseline">
               <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
-                Coursera
+                Coursera | University of Michigan
               </h3>
 
               <ul className="text-sm md:text-base">
@@ -101,7 +106,7 @@ const Certifications = () => {
 
             <div className="flex flex-col justify-between items-baseline">
               <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
-                Coursera
+                Coursera | Stanford Online | DeepLearning.AI
               </h3>
 
               <ul className="text-sm md:text-base">
@@ -111,6 +116,58 @@ const Certifications = () => {
               <a
                 className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer"
                 href={"https://www.coursera.org/verify/GRDXGT2U7PSZ"}
+                target="_blank"
+              >
+                View Credentials
+              </a>
+            </div>
+          </div>
+
+          {/*Advanced Learning Algorithms*/}
+
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">
+              Advanced Learning Algorithms
+            </h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                Coursera | Stanford Online | DeepLearning.AI
+              </h3>
+
+              <ul className="text-sm md:text-base">
+                Credential ID: 7JM7WJBPCECV
+              </ul>
+              <br />
+              <a
+                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer"
+                href={"https://www.coursera.org/verify/7JM7WJBPCECV"}
+                target="_blank"
+              >
+                View Credentials
+              </a>
+            </div>
+          </div>
+
+          {/*Unsupervised Learning, Recommenders, Reinforcement Learning*/}
+
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">
+              Unsupervised Learning, Recommenders, Reinforcement Learning
+            </h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                Coursera | Stanford Online | DeepLearning.AI
+              </h3>
+
+              <ul className="text-sm md:text-base">
+                Credential ID: X49R5AVKRHEH
+              </ul>
+              <br />
+              <a
+                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer"
+                href={"https://www.coursera.org/verify/X49R5AVKRHEH"}
                 target="_blank"
               >
                 View Credentials
