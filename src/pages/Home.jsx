09f22c939db5ec3f5661c2192ca25c195358b1e0
Akshat <akshat26april@gmail.com>
2023-03-28 21:47:17 +0530

    Updated Some CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,16 @@ const Home = () => {
       name="home"
       className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg h-screen md:grid md:grid-cols-2 flex flex-col md:gap-8 mx-auto pt-16 px-8 md:px-4 pb-6 items-center">
+      <div className="max-w-screen-lg h-screen md:grid md:grid-cols-2 flex flex-col md:gap-28 mx-auto pt-16 px-8 md:px-4 pb-6 items-center">
         <>
           <div>
-            <h1 className="pt-6 pb-4 text-4xl md:text-5xl font-bold text-white">
+            <h1 className="pt-6 pb-4 text-4xl font-bold text-white">
               I'm Akshat
             </h1>
-            <div className="text-5xl md:text-6xl py-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+            <div className="text-5xl py-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
               A Software Developer
             </div>
-            <p className="sm:text-xl text-justify text-gray-500 py-8 max-w-md">
+            <p className="sm:text-lg text-justify text-gray-500 py-8 max-w-md">
               I have 2 years of experience building and designing software.
               Currently, I love to work on web applications using technologies
               like HTML, CSS, ReactJS etc.
@@ -25,7 +25,7 @@ const Home = () => {
               <a
                 href="https://drive.google.com/uc?export=download&id=1NQ2M8RnVCW4b5ijPRQIN0wDOuySswrjW"
                 target="_blank"
-                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg duration-300 hover:scale-105 "
+                className="group text-white w-fit px-4 py-2 my-1 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg duration-300 hover:scale-105 "
               >
                 Resume
               </a>
